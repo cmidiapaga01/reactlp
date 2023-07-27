c44ece5e5d6e698588031d2c3dee4e319c99ad7c
jcyepesgarcia <jcyepesgarcia@gmail.com>
2023-07-27 16:57:31 -0500

    Features

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/header/Header";
 import Services from "./components/services/Services";
 import About from './components/about/About'
 import Qualities from './components/qualities/Qualities'
+import Features from './components/features/Features'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Services/>
       <About/>
       <Qualities/>
+      <Features/>
     </div>
   );
 }
---
+++ src/components/features/Collapsible.jsx
@@ -0,0 +1,34 @@
+import Raect, { useState } from "react";
+import "./Collapsible.css";
+import { BsPlusLg } from "react-icons/bs";
+
+const gradient = "url(#blue-gradient)";
+
+const Collapsible = ({ feature }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="item">
+      <div
+        className={`item-head flex flex-between bg-dark ${
+          open ? "item-head-border" : null
+        }`}
+        onClick={() => setOpen(!open)}
+      >
+        <h6 className="fs-20 fw-4 text-grey">{feature.title}</h6>
+        <button
+          type="button"
+          className={`item-icon flex ${open ? "item-icon-rotate" : null}`}
+        >
+          <BsPlusLg style={{ fill: gradient }} size={32} />
+        </button>
+      </div>
+
+      <div className={`item-body ${open ? "item-body-show" : null}`}>
+        <p className="text">{feature.text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Collapsible;
